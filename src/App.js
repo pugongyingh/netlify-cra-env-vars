78c47b471ebd0adc04458deaa1691943d1d40768
pugongyingh <pgyhh@sina.cn>
2019-09-19 20:08:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">Environment Variables in a Create React App on Netlify</h1>
         </header>
         <p className="App-intro">
-          To get 99999888started, edit <code>src/App.js</code> and commit to your repo.
+          To get 777started, edit <code>src/App.js</code> and commit to your repo.
         </p>
       <script type="text/javascript">
 
@@ -27,7 +27,7 @@ class App extends Component {
         <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
         <p>TOML_ENV_VAR value is "{process.env.TOML_ENV_VAR}"</p>
         <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV_VAR}"</p>
-      
+        <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV}"</p>
       <p>TOML_ENV value is "{process.env.TOML_ENV}"</p>
       <p>NODE_ENVVVVV value is "{process.env.NODE_ENVVVVV}"</p>
       </div>
